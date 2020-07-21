955a91ac2307acf071c8771353700345384db433
Aakash <9267aakashsharma@gmail.com>
2020-07-22 01:37:20 +0530

    fix(SingleNote): Added styles
    Signed-off-by: Aakash <9267aakashsharma@gmail.com>

--- src/pages/Dashboard/components/SingleNote/SingleNote.tsx
+++ src/pages/Dashboard/components/SingleNote/SingleNote.tsx
@@ -1,13 +1,27 @@
 import React from "react";
 import { Note } from "../../../../models/note";
 import "./SingleNote.scss";
+import { NoteOptions } from "../../../../shared/components";
+import { MdCheckCircle } from "react-icons/md";
+const Pushpin = require("../../../../assets/svg/push_pin.svg") as string;
 
 const SingleNote = ({ title, content }: Note) => {
   return (
     <div className="payO-note">
-      <h2>{title}</h2>
+      <div className="check-icon">
+        <MdCheckCircle />
+      </div>
+      <h2>
+        {title}
+        <span>
+          <img src={Pushpin} />
+        </span>
+      </h2>
       <div className="content">
         <p>{content}</p>
+      </div>
+      <div className="noteoptions">
+        <NoteOptions />
       </div>
     </div>
   );
