bfdf352baa627f37d1a0c7cbd20a5272c3032b01
Aakash <9267aakashsharma@gmail.com>
2020-07-22 17:31:59 +0530

    fix(SingleNote): Use imgUrl to note
    Signed-off-by: Aakash <9267aakashsharma@gmail.com>

--- src/pages/Dashboard/components/SingleNote/SingleNote.tsx
+++ src/pages/Dashboard/components/SingleNote/SingleNote.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Note } from "../../../../models/note";
 import "./SingleNote.scss";
 import { NoteOptions } from "../../../../shared/components";
 import { MdCheckCircle } from "react-icons/md";
+import Linkify from "react-linkify";
 const Pushpin = require("../../../../assets/svg/push_pin.svg") as string;
 const PushpinBlack = require("../../../../assets/svg/push_pin-black.svg") as string;
 
@@ -11,7 +12,14 @@ type Props = {
   togglePinned: (id: string) => void;
 };
 
+const images: string[] = [];
+
 const SingleNote = (props: Props) => {
+  const [imageFile, setImageFile] = useState(images);
+  const handleFile = (file: string) => {
+    setImageFile([...imageFile, file]);
+  };
+
   return (
     <div className="payO-note">
       <div className="check-icon">
@@ -32,10 +40,16 @@ const SingleNote = (props: Props) => {
         </span>
       </h2>
       <div className="content">
-        <p>{props.note.content}</p>
+        <Linkify>{props.note.content}</Linkify>
+        {props.note.imageUrl.map((image: string, index: number) => {
+          return <img src={image} alt={image} key={index} />;
+        })}
+        {imageFile.map((image: string, index: number) => {
+          return <img src={image} alt={image} key={index} />;
+        })}
       </div>
       <div className="noteoptions">
-        <NoteOptions />
+        <NoteOptions handleFile={handleFile} />
       </div>
     </div>
   );
