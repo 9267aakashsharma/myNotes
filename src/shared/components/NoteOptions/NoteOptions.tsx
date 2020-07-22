fc2b2be29249539bb0397a212149350b5a3fcba7
Aakash <9267aakashsharma@gmail.com>
2020-07-22 17:31:18 +0530

    feat(NoteOptions): Add file from local device
    Signed-off-by: Aakash <9267aakashsharma@gmail.com>

--- src/shared/components/NoteOptions/NoteOptions.tsx
+++ src/shared/components/NoteOptions/NoteOptions.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import {
   MdAddAlert,
   MdPersonAdd,
@@ -9,7 +9,26 @@ import {
 } from "react-icons/md";
 import "./NoteOptions.scss";
 
-const NoteOptions = () => {
+const NoteOptions = (props: any) => {
+  const [deleteNote, setDeleteNote] = useState(false);
+  const hiddenImageFileInput = useRef<HTMLInputElement>(null);
+
+  const handleImageClick = (event: React.MouseEvent<HTMLInputElement>) => {
+    hiddenImageFileInput.current?.click();
+  };
+
+  const handleImageChange = (event: any) => {
+    const fileUpload = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(fileUpload);
+    reader.onload = () => {
+      props.handleFile(reader.result);
+    };
+    reader.onerror = (err) => {
+      console.error(err);
+    };
+  };
+
   return (
     <div className="payO-noteoptions">
       <div>
@@ -21,14 +40,26 @@ const NoteOptions = () => {
       <div>
         <MdPalette />
       </div>
-      <div>
+      <div onClick={handleImageClick}>
+        <input
+          type="file"
+          ref={hiddenImageFileInput}
+          onChange={handleImageChange}
+          style={{ display: "none" }}
+        />
         <MdImage />
       </div>
       <div>
         <MdArchive />
       </div>
-      <div>
+      <div
+        onClick={() => {
+          setDeleteNote(!deleteNote);
+        }}
+        className="more-option"
+      >
         <MdMoreVert />
+        {deleteNote && <span>Delete</span>}
       </div>
     </div>
   );
